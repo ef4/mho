2202cd958ca8f2d323767abc6d728a4eeda6fac4
Edward Faulkner <ef@alum.mit.edu>
2021-03-11 16:12:11 -0500

    note

--- ember-app/app/app.js
+++ ember-app/app/app.js
@@ -3,11 +3,13 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-app/config/environment';
 
+// TODO: get embroider's staticHelper tracing to do this automatically
 import PageTitle from 'ember-page-title/helpers/page-title';
 window.define('ember-app/helpers/page-title', function () {
   return PageTitle;
 });
 
+// TODO: get embroider's staticComponent tracing to do this automatically
 import WelcomePage from 'ember-welcome-page/components/welcome-page';
 window.define('ember-app/components/welcome-page', function () {
   return WelcomePage;
